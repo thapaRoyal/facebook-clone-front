028a4de8bf8e3e5e948bd5177050832fc2508597
thapaRoyal <thaparoyal17@gmail.com>
2022-11-12 19:41:09 +0545

    React router dom setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+import Profile from './pages/profile';
+
 function App() {
-  return <div>welcome to frontend</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/login" element={<Login />} exact />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
