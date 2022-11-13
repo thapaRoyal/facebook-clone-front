9fc78a1a768673f6a3a0b6ea4002e983b5d9ebf1
thapaRoyal <thaparoyal17@gmail.com>
2022-11-13 20:09:00 +0545

    using formik

---
+++ src/components/Inputs/LoginInput/index.js
@@ -0,0 +1,20 @@
+import { useField } from 'formik';
+import React from 'react';
+import './style.css';
+
+const LoginInput = ({ placeholder, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div className="input_wrap">
+      <input
+        type={field.type}
+        name={field.name}
+        placeholder={placeholder}
+        {...field}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default LoginInput;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import { Form, Formik } from 'formik';
 import { Link } from 'react-router-dom';
+import LoginInput from '../../components/Inputs/LoginInput';
+
+const loginInfos = { email: '', password: '' };
 
 const Login = () => {
+  const [login, setLogin] = useState(loginInfos);
+  const { email, password } = login;
+
+  const handleLoginChange = (e) => {
+    const { name, value } = e.target;
+    setLogin({ ...login, [name]: value });
+  };
+
   return (
     <div className="login">
       <div className="login_wrapper">
@@ -17,11 +28,27 @@ const Login = () => {
           </div>
           <div className="login_2">
             <div className="login_2_wrap">
-              <Formik>
+              <Formik
+                enableReinitialize
+                initialValues={{
+                  email,
+                  password,
+                }}
+              >
                 {(formik) => (
                   <Form>
-                    <input type="text" />
-                    <input type="text" />
+                    <LoginInput
+                      type="text"
+                      name="email"
+                      placeholder="Email Address or Phone Number"
+                      onChange={handleLoginChange}
+                    />
+                    <LoginInput
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      onChange={handleLoginChange}
+                    />
                     <button type="submit" className="blue_btn">
                       Login
                     </button>
