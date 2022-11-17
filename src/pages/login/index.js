abc16793f32415b14f8f13dffeb4221539fddb6c
thapaRoyal <thaparoyal17@gmail.com>
2022-11-17 19:19:53 +0545

    style fix

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -71,7 +71,7 @@ const Login = () => {
               <button className="blue_btn open_signup">Create Account</button>
             </div>
             <Link to="/" className="sign_extra">
-              <b>Craete a Page</b> for a celebrity, brand or business.
+              <b>Create a Page</b> for a celebrity, brand or business.
             </Link>
           </div>
         </div>
